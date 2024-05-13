6f28e4c0e1267983dd7349c48a47c78c0255d3b4
Bennyyoung <benjamineffiong124@yahoo.com>
2024-05-13 10:17:25 +0100

    Feat
    1.) Finished Notification
    
    2.) Created a LayoutWithoutScrollView to stop this error VirtualizedLists should never be nested inside plain ScrollViews with the same orientation because it can break windowing and other functionality

--- src/screen/Dashboard/NotificationPage/NotificationPage.jsx
+++ src/screen/Dashboard/NotificationPage/NotificationPage.jsx
@@ -1,104 +1,84 @@
 import { StyleSheet, Text, View, FlatList, Image } from 'react-native'
 import React from 'react'
-import { SafeAreaView } from 'react-native-safe-area-context';
-import HeaderNavigation from '../../../component/HeaderNavigation';
-import food from "assets/Food1.png";
-import { Button } from 'react-native-paper';
+import LayoutWithoutScrollView from 'src/component/LayoutWithoutScrollView/LayoutWithoutScrollView';
+import LongCard from 'src/component/LongCard/LongCard';
+import ImageInNotificationOne from "assets/imageInNotificationOne.svg"
+import ImageInNotificationTwo from "assets/imageInNotificationTwo.svg"
+import ImageInNotificationThree from "assets/imageInNotificationThree.svg"
+import ImageInNotificationFour from "assets/imageInNotificationFour.svg"
+import ImageInNotificationFive from "assets/imageInNotificationFive.svg"
+import ThreeDots from "assets/threeDots.svg"
+
 
 const NotificationPage = () => {
-    const title = "Notification";
-    const uiData = [
-        { id: 1, leftImageUrl: require("assets/Food1.png"), text: "Hey, it’s time for lunch", timeText: "About 1 minute ago", rightImageUrl: require("assets/Three_dot.png"), color: "#92A3FD", line: true },
-        { id: 2, leftImageUrl: require("assets/Not_Exer1.png"), text: "Don’t miss your lowerbody workout", timeText: "About 3 hours ago", rightImageUrl: require("assets/Three_dot.png"), color: "#C58BF2", line: true },
-        { id: 3, leftImageUrl: require("assets/food2.png"), text: "Hey, let’s add some meals for your b..", timeText: "About 3 hours ago", rightImageUrl: require("assets/Three_dot.png"), color: "#92A3FD", line: true },
-        { id: 4, leftImageUrl: require("assets/Not_Exer2.png"), text: "Congratulations, You have finished A..", timeText: "29 May", rightImageUrl: require("assets/Three_dot.png"), color: "#92A3FD", line: true },
-        { id: 5, leftImageUrl: "", text: "Hey, it’s time for lunch", timeText: "8 April", rightImageUrl: require("assets/Three_dot.png"), color: "#92A3FD", line: true },
-        { id: 6, leftImageUrl: require("assets/Not_Exer1.png"), text: "Ups, You have missed your Lowerbo...", timeText: "3 April", rightImageUrl: require("assets/Three_dot.png"), color: "#C58BF2", line: false },
-    ];
+  const notifications = [
+    {
+      id: 1,
+      avatar: <ImageInNotificationOne />,
+      title: 'Hey, it’s time for lunch',
+      subtitle: 'About 1 minutes ago',
+      rightIcon: <ThreeDots />
+    },
+    {
+      id: 2,
+      avatar: <ImageInNotificationTwo />,
+      title: 'Don’t miss your lowerbody workout',
+      subtitle: 'About 3 hours ago',
+      rightIcon: <ThreeDots />
+    },
+    {
+      id: 3,
+      avatar: <ImageInNotificationThree />,
+      title: 'Hey, let’s add some meals for your b..',
+      subtitle: 'About 3 hours ago',
+      rightIcon: <ThreeDots />
+    },
+    {
+      id: 4,
+      avatar: <ImageInNotificationFour />,
+      title: 'Congratulations, You have finished A..',
+      subtitle: '29 May',
+      rightIcon: <ThreeDots />
+    },
+    {
+      id: 5,
+      avatar: <ImageInNotificationFive />,
+      title: 'Hey, it’s time for lunch',
+      subtitle: '8 April',
+      rightIcon: <ThreeDots />
+    },
+    {
+      id: 6,
+      avatar: <ImageInNotificationTwo />,
+      title: 'Ups, You have missed your Lowerbo...',
+      subtitle: '3 April',
+      rightIcon: <ThreeDots />
+    },
+  ]
 
-    const renderItem = ({ item }) => (
-        <View style={styles.itemContainer}>
-            <View style={styles.itemDataContainer}>
-                <View style={[styles.leftImageContainer, { backgroundColor: item.color }]}>
-                    {item.leftImageUrl ? <Image source={item.leftImageUrl} style={styles.leftIcon} /> : null}
-                </View>
-                <View style={[styles.textContainer, {flex:1,justifyContent:"center"}]}>
-                    <Text style={styles.text}>{item.text}</Text>
-                    <Text style={styles.timeText}>{item.timeText}</Text>
-                </View>
-                <View style={[styles.rightImageContainer, { justifyContent: 'flex-end' }]}>
-                    {item.rightImageUrl ? <Image source={item.rightImageUrl} style={styles.rightIcon} /> : null}
-                </View>
-            </View>
-            {item.line && <View style={styles.horizontalLine} />}
-        </View>
-    );
+  const renderItem = ({ item }) => (
+    <>
+      <LongCard
+        avatar={item.avatar}
+        title={item.title}
+        subtitle={item.subtitle}
+        rightIcon={item.rightIcon}
+      />
+    </>
+  )
 
-    return (
-        <SafeAreaView style={styles.container}>
-            <HeaderNavigation headerText={title} />
-            <FlatList
-                data={uiData}
-                renderItem={renderItem}
-                keyExtractor={item => item.id.toString()}
-            />
-        </SafeAreaView>
-    )
+  return (
+    <LayoutWithoutScrollView>
+      <FlatList
+        data={notifications}
+        renderItem={renderItem}
+        keyExtractor={(item) => item.id}
+        ItemSeparatorComponent={() => (
+          <View style={{ borderBottomColor: '#DDDADA', borderBottomWidth: 0.9 }}></View>
+        )}
+      />
+    </LayoutWithoutScrollView>
+  )
 }
 
 export default NotificationPage;
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        paddingHorizontal: 25,
-    },
-    itemContainer: {
-        flexDirection: 'column',
-        paddingVertical: 10,
-        gap:15,
-    },
-    itemDataContainer: {
-        flexDirection: "row",
-        alignItems: "center",
-        gap: 25
-    },
-    leftIcon: {
-        width: 22,
-        height: 22,
-    },
-    textContainer: {
-        flexDirection: "column",
-    },
-    text: {
-        flex: 1,
-    },
-    timeText: {
-        marginRight: 10,
-    },
-    rightImageContainer: {
-        width: 40,
-        height: 40,
-        borderRadius: 50,
-        backgroundColor: "white",
-        alignItems: "center",
-        justifyContent: "center",
-    },
-    leftImageContainer: {
-        width: 40,
-        height: 40,
-        borderRadius: 50,
-        justifyContent: "center",
-        alignItems: "center",
-    },
-    rightIcon: {
-        width: 22,
-        height: 22,
-    },
-    horizontalLine: {
-        borderBottomColor: '#DDDADA',
-        borderBottomWidth: 1,
-        width: "100%",
-        //marginVertical:5
-    },
-});

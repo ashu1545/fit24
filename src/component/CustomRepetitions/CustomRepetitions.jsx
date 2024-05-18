cdf271198e4922d70542f84dbdc55f2fe81d8288
Bennyyoung <benjamineffiong124@yahoo.com>
2024-05-18 14:46:37 +0100

    feat
    1.) Finished Jumping
    Jack

--- src/component/CustomRepetitions/CustomRepetitions.jsx
+++ src/component/CustomRepetitions/CustomRepetitions.jsx
@@ -1,16 +1,19 @@
 import { View, Text } from "react-native"
 
-const CustomRepetitions = ({ fireImage, caloriesBurn, number, times, color = '#EB8F63' }) => {
+const CustomRepetitions = ({ fireImage, caloriesBurn, number, times, color = '#EB8F63', paddingLeft, marginRight, horizontalLine }) => {
 
     return (
-        <View style={{ flexDirection: 'row' }}>
-            <View style={{ flexDirection: 'row' }}>
-                {fireImage}
-                <Text style={{ color: color }}>{caloriesBurn}</Text>
+        <>
+            {horizontalLine}
+            <View style={{ flexDirection: 'row', alignItems: 'center', paddingLeft: paddingLeft, marginRight: marginRight }}>
+                <View style={{ flexDirection: 'row' }}>
+                    {fireImage}
+                    <Text style={{ color: color, lineHeight: 15, fontSize: 10, fontWeight: "regular", marginRight: 10 }}>{caloriesBurn}</Text>
+                </View>
+                <Text style={{ color: color, lineHeight: 36, fontSize: 24, fontWeight: 'medium', marginRight: 10 }}>{number}</Text>
+                <Text style={{ color: color, lineHeight: 24, fontSize: 16, fontWeight: 'regular' }}>{times}</Text>
             </View>
-            <Text style={{ color: color }}>{number}</Text>
-            <Text style={{ color: color }}>{times}</Text>
-        </View>
+        </>
     )
 }
 
